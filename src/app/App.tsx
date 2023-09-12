1ce5f89ef09f5a69ca3fb9356d3ea4538a36d550
Stanislav Khokhlov <whitee123@mail.ru>
2023-09-12 17:36:38 +0500

    feat: navbar, link themes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,18 +3,18 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Navbar } from 'widgets/Navbar';
 import './styles/index.scss';
 
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
-  
+
   return (
-    <div className={classNames('app', {}, [theme])}>
+    <div className={classNames('app', {}, [theme])}>      
+      <Navbar />
       <button onClick={toggleTheme}>Toggle theme</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
-      <AppRouter />
+      <AppRouter />      
     </div>
   );
 };
