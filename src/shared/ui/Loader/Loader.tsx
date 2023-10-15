0aa9e5615bedd855ab8fa46e3371880599d36b57
Stanislav Khokhlov <whitee123@mail.ru>
2023-10-15 17:15:26 +0500

    feat: page loader, page 404

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import './Loader.scss';
+
+interface LoaderProps {
+  className?: string;
+}
+
+export const Loader = ({ className }: LoaderProps) => (
+    <div className={classNames('lds-hourglass', {}, [className])}>
+        <div />
+    </div>
+);
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import cls from './Sidebar.module.scss';
@@ -19,7 +20,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
 
     return (
         <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-            <button type="button" onClick={onToggle}>{t('Свернуть')}</button>
+            <Button onClick={onToggle}>{t('Свернуть')}</Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
